386d63a151f3e4a913a02b085c0423b7f75b5f65
SharpCoder <joshua@debuggle.com>
2014-02-24 23:02:58 -0800

    Finished creating simple graph.

---
+++ CUDAGraph-1/SimpleGraph/Graph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraph
+{
+	public class Graph
+	{
+		Random rand;
+		Node inputNode;
+		public List<Node> flattened = new List<Node>();
+		public List<double> benchmark = new List<double>();
+		Stopwatch bm = new Stopwatch();
+
+		public Graph(int[] topology)
+		{
+			if (topology == null || topology.Length == 0) return;
+
+			this.rand = new Random(424242);
+			inputNode = new Node(topology[0], this.rand);
+			flattened.Add(inputNode);
+			deploy(new Node[] { inputNode }, topology, 1);
+		}
+
+		public float Evaluate(float[] input)
+		{
+			bm.Reset();
+			bm.Start();
+			if (inputNode == null || input == null || input.Length == 0) return 0f;
+			for (int i = 0; i < input.Length; i++)
+				inputNode.Push(input[i]);
+
+			float output = inputNode.Eval();
+			Console.ForegroundColor = ConsoleColor.Green;
+			double benchmarkValue = bm.ElapsedTicks;
+			Console.Write("\n" + output + " @ " + benchmarkValue + " ticks");
+			benchmark.Add(benchmarkValue);
+			Console.ResetColor();
+			return output;
+		}
+
+		private void deploy(Node[] parents, int[] topology, int index)
+		{
+			if (index + 1 > topology.Length)
+			{
+				// Output layer.
+				return;
+			}
+
+			Node[] layer = new Node[topology[index]];
+			for (int i = 0; i < layer.Length; i++)
+			{
+				layer[i] = new Node(parents.Length, this.rand);
+				flattened.Add(layer[i]);
+			}
+
+			// Iterate over the parents.
+			for (int i = 0; i < parents.Length; i++)
+			{
+				parents[i].children = layer;
+			}
+
+			// Recurse.
+			deploy(layer, topology, index + 1);
+		}
+	}
+}
--- CUDAGraph-1/SimpleGraph/Node.cs
+++ CUDAGraph-1/SimpleGraph/Node.cs
@@ -8,5 +8,47 @@ namespace SimpleGraph
 {
 	public class Node
 	{
+		internal float weight;
+		internal float[] inputs;
+		int index = 0;
+		public Node[] children;
+
+		public Node(int inputCount, Random rand)
+		{
+			if (rand == null) return;
+			this.inputs = new float[inputCount];
+			this.weight = (float)(rand.NextDouble() - rand.NextDouble());
+			this.children = null;
+		}
+
+		public void Push(float val)
+		{
+			if (index + 1 > inputs.Length) index = 0;
+			this.inputs[index++] = val;
+		}
+
+		public float Eval()
+		{
+			if (index != inputs.Length) return 0f;
+
+			float outVal = 0f;
+			for (int i = 0; i < inputs.Length; i++)
+			{
+				outVal += inputs[i] * weight;
+			}
+
+			if (children != null)
+			{
+				float output = 0f;
+				for (int i = 0; i < children.Length; i++)
+				{
+					children[i].Push(outVal);
+					output += children[i].Eval();
+				}
+				return output;
+			}
+
+			return outVal;
+		}
 	}
 }
