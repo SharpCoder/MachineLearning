d4888ffeb188ee35de431ab0deae8ab19c8667ef
SharpCoder <joshua@debuggle.com>
2014-02-24 21:26:57 -0800

    Updated readme, prototyped the thread algorithm.

--- CUDAGraph-1/ConsoleHook/Program.cs
+++ CUDAGraph-1/ConsoleHook/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleHook
 	{
 		static void Main(string[] args)
 		{
+			Graph graph = new Graph(new int[] { 1, 20, 100, 1 });
+
+			while (true)
+			{
+				Console.Write("Enter a value: ");
+				var strInput = Console.ReadLine();
+				float input = 0f;
+				if (float.TryParse(strInput, out input))
+				{
+					graph.Input(new float[] { input });
+				}
+			}
+
+			Console.WriteLine("Program Terminated. \nPress any key to continue.");
+			Console.ReadKey();
 		}
 	}
 }
