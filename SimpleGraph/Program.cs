e0b7e3a9e1d7f2c1ef647f8228eec5e82eb0b03e
SharpCoder <joshua@debuggle.com>
2014-02-26 21:03:02 -0800

    Switching the project around.

---
+++ SimpleGraph/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraph
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Graph graph = new Graph(new int[] { 1, 200, 200, 100, 1 });
+
+			Console.WriteLine("Performing basic graph traversal");
+
+			for (int i = 0; i < 11; i++)
+			{
+				graph.Evaluate(new float[] { 1.0f });
+			}
+
+			// NOTE: The following code is for benchmarking purposes.
+			// I skip the first iteration to account for initialization costs.
+			double avg = 0d;
+			for ( int i = 1; i < graph.benchmark.Count; i++ )
+				avg += graph.benchmark[i];
+			avg /= (graph.benchmark.Count - 1);
+
+			Console.WriteLine("Average ticks: " + avg);
+			Console.WriteLine("Program Terminated. Press any key to continue.");
+			Console.ReadKey();
+		}
+	}
+}
